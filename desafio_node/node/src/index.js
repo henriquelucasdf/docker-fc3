06fbc3e131e5c56c357aeacc34ae1aaa75f36722
henriquelucasdf <henriquelucasdf@gmail.com>
2022-09-09 13:35:45 -0300

    added html forms and sql query

--- desafio_node/node/src/index.js
+++ desafio_node/node/src/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser');
+var arr = [
+    "[NOMES CADASTRADOS]",
+  ];
 const port = 3000
 const config = {
     host: 'db_node',
@@ -7,17 +11,41 @@ const config = {
     password: 'root',
     database: 'nodedb'
 };
+
 const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) values('Henrique')`
-connection.query(sql)
-connection.end()
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({
+   extended: false
+}));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('<h1> Full Cycle </h1>')
-})
+app.get('/', function(req, res){
 
-app.listen(port, ()=> {
-    console.log('Rodando na porta ' + port)
-})
+    res.sendFile(__dirname + '/simple_form.html'); 
+
+});
+
+app.post('/',function(req,res){
+    var username = req.body.username;
+    var sqli = `INSERT INTO people(name) values('` + username + `')`
+    connection.query(sqli)
+   
+    connection.query("SELECT name FROM people", function (err, result, fields) {
+        Object.keys(result).forEach(function(key) {
+            var row = result[key];
+
+            arr.push(row.name)
+            
+          });
+          res.send(arr.join('  |  '));
+          
+        });
+    
+    });
+      
+
+
+    
+app.listen(port);
